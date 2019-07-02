0cfa07835352563943886e2c3bef5f141037c097
FengYuFei <2685757360@qq.com>
2019-07-02 17:57:01 +0800

    PMSS 商品model添加注释

--- PreciousMetalSalesSystem/PreciousMetalSalesSystem/Models/PMSSGoodsModel.h
+++ PreciousMetalSalesSystem/PreciousMetalSalesSystem/Models/PMSSGoodsModel.h
@@ -29,16 +29,44 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface PMSSGoodsModel : NSObject
 
+/**
+ 商品名字
+ */
 @property (nonatomic, copy) NSString *goodName;
 
+/**
+ 商品Id
+ */
 @property (nonatomic, copy) NSString *goodId;
 
+/**
+ 商品价格
+ */
 @property (nonatomic, copy) NSString *goodPrice;
 
+/**
+ 商品单位
+ */
 @property (nonatomic, copy) NSString *goodUnit;
 
+
+/**
+ 商品满减优惠集合
+ FullSubTypeThree,     //满三千减350
+ FullSubTypeTwo,       //  满两千减30
+ FullSubTypeOne,      //满一千减10
+ FullSubTypeSubHalf,      //第3件半价（买3件及以上，其中1件半价）
+ FullSubTypeSubOne,      //满3送1（买4件及以上，其中1件免费）
+ */
 @property (nonatomic, strong)NSArray *fullSubArray;
 
+
+/**
+ 商品打折券使用规则
+ DiscountTypeNone,     //默认为不可用
+ DiscountType9,        //可用9折券
+ DiscountType95,       // 可用95折券
+ */
 @property (nonatomic, assign)DiscountType disType;
 
 
