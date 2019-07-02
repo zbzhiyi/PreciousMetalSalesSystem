6a51f98086c1ed7de9fa078f2714b596e4282592
zhangbo <zhangbo03@zybank.com>
2019-07-02 16:12:37 +0800

    添加Models文件夹

---
+++ PreciousMetalSalesSystem/PreciousMetalSalesSystem/Models/PMSSCustomerModel.h
@@ -0,0 +1,19 @@
+//
+//  PMSSCustomerModel.h
+//  PreciousMetalSalesSystem
+//
+//  Created by ZB on 2019/7/2.
+//  Copyright © 2019年 ZB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PMSSCustomerModel : NSObject
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PreciousMetalSalesSystem/PreciousMetalSalesSystem/Models/PMSSCustomerModel.m
@@ -0,0 +1,13 @@
+//
+//  PMSSCustomerModel.m
+//  PreciousMetalSalesSystem
+//
+//  Created by ZB on 2019/7/2.
+//  Copyright © 2019年 ZB. All rights reserved.
+//
+
+#import "PMSSCustomerModel.h"
+
+@implementation PMSSCustomerModel
+
+@end
